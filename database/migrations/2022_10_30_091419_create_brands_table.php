986d87b8cda0aef4af386b1ca6bf06feb53dfa20
IROZEN-DEV1 <rasel.irozen@gmail.com>
2023-10-10 17:10:13 +0600

    brand table added

---
+++ database/migrations/2022_10_30_091419_create_brands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand_name');
+            $table->string('image')->nullable();
+            $table->tinyInteger('status')->default(1)->comment('0=inactive,1=active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
