5dafabe188d3a912efb75e97d8350755b8deeeb5
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-10-23 17:11:02 +0600

    fix

--- resources/views/webend/ecommerce/merchant/product/index.blade.php
+++ resources/views/webend/ecommerce/merchant/product/index.blade.php
@@ -77,39 +77,41 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($products as $product)
-                            <tr class="hide_row{{$product->id}} bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="px-2 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap border-r text-center">
-                                    {{ $loop->iteration }}
-                                </td>
+                       @if(!is_null($products))
+                           @foreach($products as $product)
+                               <tr class="hide_row{{$product->id}} bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                   <td class="px-2 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap border-r text-center">
+                                       {{ $loop->iteration }}
+                                   </td>
 
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    <img style="width: 150px;height: 60px" src="{{asset('uploads/product/small/'.$product->thumbnail) }}">
-                                </td>
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    {{$product->merchant->name}}
-                                </td>
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    {{$product->title}}
-                                </td>
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    {{price_format($product->current_price)}}
-                                </td>
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    {{$product->current_coin}}
-                                </td>
-                                <td class="px-2 py-4 text-black border-r text-center">
-                                    {{$product->stocks->sum('quantity')}}
-                                </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       <img style="width: 150px;height: 60px" src="{{asset('uploads/product/small/'.$product->thumbnail) }}">
+                                   </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       {{$product->merchant->name}}
+                                   </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       {{$product->title}}
+                                   </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       {{price_format($product->current_price)}}
+                                   </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       {{$product->current_coin}}
+                                   </td>
+                                   <td class="px-2 py-4 text-black border-r text-center">
+                                       {{$product->stocks->sum('quantity')}}
+                                   </td>
 
-                                <td class="whitespace-nowrap px-2 py-4 text-black border-r text-center">
-                                    <a href="{{route('product.product_detail',$product->slug)}}" class="text-white bg-indigo-400  hover:bg-sky-500 transition-all ease-in-out font-medium rounded-md text-sm inline-flex items-center px-5 py-2 text-center">
-                                        <svg class="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-                                        View
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
+                                   <td class="whitespace-nowrap px-2 py-4 text-black border-r text-center">
+                                       <a href="{{route('product.product_detail',$product->slug)}}" class="text-white bg-indigo-400  hover:bg-sky-500 transition-all ease-in-out font-medium rounded-md text-sm inline-flex items-center px-5 py-2 text-center">
+                                           <svg class="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
+                                           View
+                                       </a>
+                                   </td>
+                               </tr>
+                           @endforeach
+                       @endif
                         </tbody>
                     </table>
                 </div>
