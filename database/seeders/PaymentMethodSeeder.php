d3b2b035d8daa9f068a763fe49b7c531ca117bf4
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-10-29 15:05:02 +0600

    Payment method seeder

---
+++ database/seeders/PaymentMethodSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PaymentMethodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('payment_methods')->insert([
+                [
+                    'payment_method_name' => 'UPI (Google Pay/ PhonePe)',
+                    'status' => 0,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'payment_method_name' => 'Wallet',
+                    'status' => 0,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'payment_method_name' => 'Debit/Credit Card',
+                    'status' => 0,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'payment_method_name' => 'Net Banking',
+                    'status' => 1,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'payment_method_name' => 'Cash On Delivery',
+                    'status' => 1,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'payment_method_name' => 'Mobile Banking',
+                    'status' => 1,
+                    'created_at' => Carbon::now()
+                ]
+        ]
+        );
+    }
+}
