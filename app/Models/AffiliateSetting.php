2a93227e635229365e956a0550dd5491ef3ba59f
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-11-05 11:29:25 +0600

    include AffiliateSettingSeeder

--- app/Models/AffiliateSetting.php
+++ app/Models/AffiliateSetting.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class AffiliateSetting extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
---
+++ database/seeders/AffiliateSettingSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class AffiliateSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('affiliate_settings')->insert(
+            [
+                'company_commission' => 25,
+                'generation_commission' => 20,
+                'game_asset_commission' => 10,
+                'top_seller_commission' => 30,
+                'share_holder_commission' => 15,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now()
+
+            ]
+        );
+    }
+}
