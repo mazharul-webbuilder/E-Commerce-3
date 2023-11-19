2f8f0b570806c8f07e6f969b2e7a398945621179
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-11-19 13:28:10 +0600

    users table improve

--- resources/views/webend/user/user.blade.php
+++ resources/views/webend/user/user.blade.php
@@ -197,9 +197,8 @@
 
                             @foreach ($users as $user)
                                 <tr
-                                    class="bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <td
-                                        class="px-2 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap border-r text-center">
+                                    class="bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50">
+                                    <td class="px-2 py-4 text-black border-r text-center">
                                         {{ $loop->iteration }}
                                     </td>
                                     <td class="px-2 py-4 text-black border-r text-center">
